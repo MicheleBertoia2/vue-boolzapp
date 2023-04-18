960bf0965e965a7ad4917ff9e96a07a73a137a74
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-18 16:40:32 +0200

    [preparazione milestone 5]

---
+++ milestone5/assets/js/script.js
@@ -0,0 +1,63 @@
+import contacts from './db.js'
+const {createApp} = Vue;
+const dt = luxon.DateTime;
+
+createApp({
+
+  data() {
+
+    return{
+      contacts,
+      activeUser : 0,
+      newMsgString : '',
+      dateSent : dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS),
+      searchStr : '',
+    }    
+  },
+
+  methods : {
+    selectUser(index){
+      this.activeUser = index
+      console.log(contacts[index].visible);
+    },
+
+    sendMessage(){
+      const newMsg = {
+        date: this.dateSent,
+        message: this.newMsgString,
+        status: 'sent'
+      }
+      this.contacts[this.activeUser].messages.push(newMsg)
+      this.newMsgString = ''
+
+      setTimeout(() =>{
+        const newMsg = {
+          date: this.dateSent,
+          message: 'ok',
+          status: 'received'
+        }
+        this.contacts[this.activeUser].messages.push(newMsg)
+      },1000)
+    },
+
+    filterSearch(){
+      this.contacts.forEach(contact => {
+        if(contact.name.toLowerCase().includes(this.searchStr.toLowerCase())){
+          contact.visible = true;
+        } else {
+          contact.visible = false;
+        }
+      });
+    }
+
+    
+  },
+
+  computed : {
+    
+  },
+
+  mounted(){    
+  }
+
+}).mount('#app')
