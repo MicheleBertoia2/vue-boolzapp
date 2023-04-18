6bea15ba3401929719c9207ca1ed8ff8f9f93fc5
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-18 14:38:01 +0200

    [add] funzione per aggiungere messaggi alla chat

--- milestone3/assets/js/script.js
+++ milestone3/assets/js/script.js
@@ -8,12 +8,23 @@ createApp({
     return{
       contacts,
       activeUser : 0,
+      newMsgString : '',
     }    
   },
 
   methods : {
     selectUser(index){
       this.activeUser = index
+    },
+
+    sendMessage(){
+      const newMsg = {
+        date: '10/01/2020 15:30:55',
+        message: this.newMsgString,
+        status: 'sent'
+      }
+      this.contacts[this.activeUser].messages.push(newMsg)
+      this.newMsgString = ''
     }
   },
 
